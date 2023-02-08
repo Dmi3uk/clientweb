d74e080f38f104d80142295c63ee39af70b20d74
dmi3uk <Dmi3uk@yandex.ru>
2023-02-08 16:55:58 +0200

    init

---
+++ src/main/java/com/daky/registerclientservice/clientweb/ClientwebApplication.java
@@ -0,0 +1,13 @@
+package com.daky.registerclientservice.clientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daky/registerclientservice/clientweb/ClientwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daky.registerclientservice.clientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
